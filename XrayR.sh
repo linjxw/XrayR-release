a94b54a2336a3697dbe79d269afb581c09f32f51
linjxw <127081938+linjxw@users.noreply.github.com>
2025-10-30 01:44:33 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -84,7 +84,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/wyusgw/XrayR-release/refs/heads/main/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/linjxw/XrayR-release/refs/heads/main/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -108,7 +108,7 @@ update() {
 #        fi
 #        return 0
 #    fi
-    bash <(curl -Ls https://raw.githubusercontent.com/wyusgw/XrayR-release/refs/heads/main/install.sh) $version
+    bash <(curl -Ls https://raw.githubusercontent.com/linjxw/XrayR-release/refs/heads/main/install.sh) $version
     if [[ $? == 0 ]]; then
         echo -e "${green}更新完成，已自动重启 XrayR，请使用 XrayR log 查看运行日志${plain}"
         exit
@@ -270,7 +270,7 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/XrayR -N --no-check-certificate https://raw.githubusercontent.com/wyusgw/XrayR-release/refs/heads/main/XrayR.sh
+    wget -O /usr/bin/XrayR -N --no-check-certificate https://raw.githubusercontent.com/linjxw/XrayR-release/refs/heads/main/XrayR.sh
     if [[ $? != 0 ]]; then
         echo ""
         echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
@@ -387,7 +387,7 @@ show_usage() {
 show_menu() {
     echo -e "
   ${green}XrayR 后端管理脚本，${plain}${red}wyusgw${plain}
---- https://github.com/wyusgw/XrayR ---
+--- https://github.com/linjxw/XrayR ---
   ${green}0.${plain} 修改配置
 ————————————————
   ${green}1.${plain} 安装 XrayR
